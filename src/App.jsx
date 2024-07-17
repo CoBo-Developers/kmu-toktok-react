d7e94b2da38526c64901066933b65ef8640ac31c
Jonghyeok <jhpmuscle@gmail.com>
2024-07-17 14:43:30 +0900

    style: aside user info

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import tagIcon from './assets/icons/tag-icon.png'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -7,6 +8,13 @@ function App() {
       <aside>
         <section className='aside-menu'>
           <h1 className='aside-title'>kmu<br/>toktok-.</h1>
+          <section className='aside-user-info'>
+            <img className='aside-user-info-icon' src={tagIcon} alt="tag-icon" />
+            <article className='aside-user-info-content'>
+              <span className='aside-id'>2022123455</span>
+              <span className='aside-logout'>로그아웃</span>
+            </article>
+          </section>
         </section>
         <section>
 
