c0572d1e24265b8203d2bf0425b2c28f68d7165e
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-07-19 02:34:38 +0900

    fix: zustand setter fix

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -3,8 +3,8 @@ import { create } from 'zustand';
 const useUserStore = create((set) => ({
   name: '',
   studentId: '',
-  setName: (name) => set({ name }),
-  setStudentId: (studentId) => set({ studentId }),
+  setName: (name) => set({ name: name }),
+  setStudentId: (studentId) => set({ studentId: studentId }),
 }));
 
 export default useUserStore;
