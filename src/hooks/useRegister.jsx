4a31500bd98b3e97b9e036e8a7f9799a6a1733fb
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-07-21 16:36:12 +0900

    feat: add register api

--- src/hooks/useRegister.jsx
+++ src/hooks/useRegister.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 import useUserStore from '../store/useUserStore';
+import { registerApi } from '../api/registerApi';
+import { setCookie } from '../utils/cookieManage';
+import { useNavigate } from 'react-router-dom';
 
 function useRegister() {
   const { name, studentId, setName, setStudentId } = useUserStore();
   const [currentStep, setCurrentStep] = useState(1);
   const [isChecked, setIsChecked] = useState(false);
+  const navigate = useNavigate();
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -23,6 +27,18 @@ function useRegister() {
       if (isChecked) {
         setCurrentStep(3);
       }
+    } else if (currentStep === 3) {
+      registerApi(name, studentId)
+        .then((response) => {
+          console.log('Register success:', response);
+          setCookie("accessToken", response.data.accessToken, 2 * 60);
+          setCookie("refreshToken", response.data.refreshToken, 24 * 14 * 60);
+          setCookie('isActive', response.data.registerStateEnum,  2 * 60);
+          navigate('/chatbot');
+        })
+        .catch((error) => {
+          console.error(error.message);
+        });
     }
   };
 
