20a7ac0abebff2c924f0c11f35b31fd8157ffb82
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-07-22 23:20:13 +0900

    feat: tracking cookies in useEffect

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,11 +1,12 @@
 
 import { useNavigate} from 'react-router-dom';
+import { useEffect} from 'react';
 import { loginApi, reissueApi } from '../api/authApi';
 import { useCookies } from 'react-cookie';
 
 function useAuth() {
   const [cookies, setCookie] = useCookies(['accessToken', 'refreshToken', 'isActive']);
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
 
   const login = (code, option) => {
     if(code && option) {
@@ -23,17 +24,31 @@ function useAuth() {
   };
 
   const reissue = () => {
-    reissueApi(cookies.refreshToken)
-      .then((response) => {
-        setCookie("accessToken", response.data.accessToken, {maxAge: 2 * 60 * 60});
-        setCookie("refreshToken", response.data.refreshToken, {maxAge: 24 * 14 * 60});
-        setCookie('isActive', response.data.registerStateEnum, {maxAge: 2 * 60 * 60});
-        navigate('/chatbot');
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
+    if (cookies.refreshToken) {
+        reissueApi(cookies.refreshToken)
+        .then((response) => {
+            setCookie("accessToken", response.data.accessToken, {maxAge: 2 * 60 * 60});
+            setCookie("refreshToken", response.data.refreshToken, {maxAge: 24 * 14 * 60});
+            setCookie('isActive', response.data.registerStateEnum, {maxAge: 2 * 60 * 60});
+            navigate('/chatbot');
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+    }
   }
+
+  useEffect(() => {
+    if (!cookies.accessToken && cookies.refreshToken) {
+      reissue();
+    }
+  }, [cookies.accessToken]);
+
+  useEffect(() => {
+    if (cookies.accessToken) {
+      navigate('/chatbot');
+    }
+  }, [cookies, navigate]);
 
   return { login, reissue };
 }
