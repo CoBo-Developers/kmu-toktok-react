9858c2a96b6599579ec07acaa846a308c3c10f91
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-08-24 01:47:09 +0900

    refactor: remove google login

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,9 +1,6 @@
 import './Login.css';
 import kakaoImg from '../assets/images/kakao_logo.png';
 import naverImg from '../assets/images/naver_logo.png';
-import googleImg from '../assets/images/google_logo.png';
-
-
 
 function Login() {
     const handleLoginBtn = (option) => {
@@ -15,9 +12,6 @@ function Login() {
             case 'naver':
                 url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${import.meta.env.VITE_APP_NAVER_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_APP_NAVER_REDIRECT_URI}`;
                 break;
-            case 'google':
-                url = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${import.meta.env.VITE_APP_GOOGLE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_APP_GOOGLE_REDIRECT_URI}&scope=https://www.googleapis.com/auth/userinfo.email`;
-
         }
         window.location.href = url;
     }
@@ -37,9 +31,6 @@ function Login() {
                         </li>
                         <li className='naver-login-btn' onClick={()=> handleLoginBtn('naver')}>
                             <img src={naverImg} alt='' />
-                        </li>
-                        <li className='google-login-btn' onClick={()=> handleLoginBtn('google')}>
-                            <img src={googleImg} alt='' />
                         </li>
                     </ul>
                     <a id="mailBtn" href="#">문의하기</a>
